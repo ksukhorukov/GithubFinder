3330ef68d664c2587a92e67406da6a7d712dfcd3
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2020-04-27 07:12:16 +0300

    GithubService pagination

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,2 +1,11 @@
 class GithubController < ApplicationController
+	def index
+		@result  = GithubService.new(github_params).search.to_json
+	end
+
+	private
+
+	def github_params
+		params.permit(:repo_name, :per_page, :page)
+	end
 end
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,20 +1,30 @@
 require 'octokit'
 
 class GithubService
-	attr_reader :repo_name, :page, :per_page, :client
+	attr_reader :repo_name, :page, :per_page, :client, :result
 
-	def initalize(repo_name, page = 1, per_page = 10)
+	def initialize(params = {})
+		@repo_name = params.fetch(:repo_name, '')
+		@per_page = params.fetch(:per_page, 100)
+		@page = params.fetch(:page, 1)
+
 		@client = Octokit::Client.new
-		@repo_name = repo_name
-		@per_page = per_page
-		@page = page
 	end
 
 	def search
-		client.search_repos(repo_name, { per_page: per_page, page: page })
+		@result ||= client.search_repos(repo_name, { per_page: per_page, page: page })
 	end
 
 	def pagination
+		total_count = search[:total_count].to_i
+		number_of_pages = (total_count / 100).ceil
+		numbers = ''
+
+		for i in 1..number_of_pages do
+  		numbers << "[#{i}] "
+		end
+
+		numbers
 	end
 end
 
