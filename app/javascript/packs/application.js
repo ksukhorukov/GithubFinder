ef60d1d96743c8a62fe5f6af7421c4fc8420eb5b
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2020-04-27 05:31:58 +0300

    bootstrap support

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,8 @@
 
 require("@rails/ujs").start()
 require("turbolinks").start()
-
+require("jquery")
+require("bootstrap")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -13,3 +14,7 @@ require("turbolinks").start()
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import '../stylesheets/application'
+
+import './bootstrap_custom.js'
