9abce34a068eee991d943ec65d3c75bfa692009c
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2020-04-27 13:06:04 +0300

    test. index page loads template correctly

--- spec/requests/github_request_spec.rb
+++ spec/requests/github_request_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe "Githubs", type: :request do
     get root_path
     expect(response).to have_http_status(:success)
   end
+
+  it "renders the index template" do
+    get root_path
+    expect(response).to render_template("index")
+  end
 end
