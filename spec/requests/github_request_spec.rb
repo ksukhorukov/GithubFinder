f33fda4d86c1ec852f1a885abaea96594c8c546d
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2020-04-27 13:13:33 +0300

    test. find repo when it exists

--- spec/requests/github_request_spec.rb
+++ spec/requests/github_request_spec.rb
@@ -10,4 +10,9 @@ RSpec.describe "Githubs", type: :request do
     get root_path
     expect(response).to render_template("index")
   end
+
+  it "successfully display repo info when the repo exists" do 
+  	get "#{root_path}?repo_name=2d931510-d99f-494a-8c67-87feb05e1594"
+  	expect(response.body).to include("Test repo for Github Finder")
+  end
 end
